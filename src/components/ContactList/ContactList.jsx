8eda462f575b7d4fb466ba0fc2d3f8c009fe58d5
KseniaLobova <128805342+KseniaLobova@users.noreply.github.com>
2023-11-17 14:23:11 +0100

    uodate

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,7 +5,7 @@ import { Item, List } from './ContactList.styled';
 export const ContactList = () => {
   const contacts = useSelector(state => state.contacts.contacts);
   const filter = useSelector(state => state.contacts.filter);
-
+  console.log(contacts);
   const visibleContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
